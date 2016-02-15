e94f4a378e1b2b34fe04484d9849b24708d1a353
David Sontag <dsontag@cs.nyu.edu>
2016-02-14 22:22:23 -0500

    code to create claims_cuis_hs_300.txt from claims_codes_hs_300.txt

---
+++ eval/convert_code_to_cui_embedding.py
@@ -0,0 +1,100 @@
+import numpy as np
+from ibc_analysis import get_icd9_cui_mappings
+
+# using best
+ingredient_to_best_ndc = {}
+with open('ingredient_drug_ndc', 'r') as infile:
+    data = infile.readlines()
+    for row in data:
+        eles = row.strip().split(' ')
+        ingredient_to_best_ndc[eles[0]] = eles[2]
+
+# using parents with smallest number of child
+ingredient_to_ndcs = {}
+with open('ingredient_ndcs', 'r') as infile:
+    data = infile.readlines()
+    for row in data:
+        eles = row.strip().split(' ')
+        ingredient_to_ndcs[eles[0]] = eles[1:]
+
+# using all parents
+ingredient_to_all_ndcs = {}
+with open('ingredient_all_ndcs', 'r') as infile:
+    data = infile.readlines()
+    for row in data:
+        eles = row.strip().split(' ')
+        ingredient_to_all_ndcs[eles[0]] = eles[1:]
+
+ibc_filenames = []
+with open('list_of_ibc_files.txt', 'r') as infile:
+    data = infile.readlines()
+    for row in data:
+        ibc_filename = row.strip()
+        ibc_filenames.append(ibc_filename)
+
+
+cui_to_icd9, icd9_to_cui = get_icd9_cui_mappings()
+for embedding_filename in ibc_filenames:
+    print embedding_filename
+    ndc_to_embeddings = {}
+    icd9_cui_to_embeddings = {}
+    with open(embedding_filename, 'r') as infile:
+        data = infile.readlines()
+        for row in data:
+            eles = row.strip().split(' ')
+            name = eles[0]
+            embedding = ' '.join(eles[1:])
+            if name[0] == 'N':
+                ndc_to_embeddings[name[2:]] = embedding
+            if name[0] == 'I':
+                if name[4:] in icd9_to_cui:
+                    cui = icd9_to_cui[name[4:]]
+                    icd9_cui_to_embeddings[cui] = embedding
+
+
+    '''
+    outfilename = 'ibc_embeddings/' + 'best_' + embedding_filename.split('/')[-1]
+    with open(outfilename, 'w') as outfile:
+        for ingredient in ingredient_to_best_ndc.keys():
+            best_ndc = ingredient_to_best_ndc[ingredient]
+            
+            outfile.write('%s %s\n' %(ingredient, ndc_to_embeddings[best_ndc]))
+    '''
+
+    outfilename = 'ibc_embeddings/' + 'more_' + embedding_filename.split('/')[-1]
+    with open(outfilename, 'w') as outfile:
+        for ingredient in ingredient_to_ndcs.keys():
+            ndcs = ingredient_to_ndcs[ingredient]
+            embeddings = []
+            for ndc in ndcs:
+                if ndc in ndc_to_embeddings:
+                    embedding = map(float, ndc_to_embeddings[ndc].split(' '))
+                    embeddings.append(embedding)
+            if len(embeddings) > 0:
+                embeddings = np.array(embeddings)
+                embedding = np.mean(embeddings, axis=0)
+                outfile.write('%s ' %(ingredient))
+                for i in range(embedding.shape[0]):
+                    outfile.write('%.6f ' %(embedding[i]))
+                outfile.write('\n')
+        for cui in icd9_cui_to_embeddings:
+            outfile.write('%s %s\n' %(cui, icd9_cui_to_embeddings[cui]))
+    
+    outfilename = 'ibc_embeddings/' + 'all_' + embedding_filename.split('/')[-1]
+    with open(outfilename, 'w') as outfile:
+        for ingredient in ingredient_to_all_ndcs.keys():
+            ndcs = ingredient_to_all_ndcs[ingredient]
+            embeddings = []
+            for ndc in ndcs:
+                if ndc in ndc_to_embeddings:
+                    embedding = map(float, ndc_to_embeddings[ndc].split(' '))
+                    embeddings.append(embedding)
+            if len(embeddings) > 0:
+                embeddings = np.array(embeddings)
+                embedding = np.mean(embeddings, axis=0)
+                outfile.write('%s ' %(ingredient))
+                for i in range(embedding.shape[0]):
+                    outfile.write('%.6f ' %(embedding[i]))
+                outfile.write('\n')
+        for cui in icd9_cui_to_embeddings:
+            outfile.write('%s %s\n' %(cui, icd9_cui_to_embeddings[cui]))
